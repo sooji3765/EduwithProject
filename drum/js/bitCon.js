2def45d977c4699054c5863dd2d3c4f37240e08c
sooji3765 <susie3765@naver.com>
2019-05-01 18:07:33 +0900

    bitcontroller 수정

--- drum/js/bitCon.js
+++ drum/js/bitCon.js
@@ -1,46 +1,16 @@
 $(document).on('keydown',pressAction);
 $(document).on('keyup',pressRemove);
-     
+
 function pressAction(event){
-    if(event['key']==='1'){
-        $("#cell1").addClass("playing");
-    }else if(event['key']==='2'){
-        $("#cell2").addClass("playing");
-    }else if(event['key']==='3'){
-        $("#cell3").addClass("playing");
-    }else if(event['key']==='4'){
-        $("#cell4").addClass("playing");
-    }else if(event['key']==='5'){
-            $("#cell5").addClass("playing");
-    }else if(event['key']==='6'){
-            $("#cell6").addClass("playing");
-    }else if(event['key']==='7'){
-            $("#cell7").addClass("playing");
-    }else if(event['key']==='8'){
-            $("#cell8").addClass("playing");
-    }else if(event['key']==='9'){
-            $("#cell9").addClass("playing");
-    }
+        if(Number(event['key'])>=1&&Number(event['key'])<=9){
+                var cell = "#cell"+event['key'];	
+                $(cell).addClass("playing");   
+        }
 }
-     
+
 function pressRemove(event){
-    if(event['key']==='1'){
-        $("#cell1").removeClass("playing");
-    }else if(event['key']==='2'){
-            $("#cell2").removeClass("playing");
-    }else if(event['key']==='3'){
-        $("#cell3").removeClass("playing");
-    }else if(event['key']==='4'){
-            $("#cell4").removeClass("playing");
-    }else if(event['key']==='5'){
-            $("#cell5").removeClass("playing");
-    }else if(event['key']==='6'){
-            $("#cell6").removeClass("playing");
-    }else if(event['key']==='7'){
-            $("#cell7").removeClass("playing");
-    }else if(event['key']==='8'){
-            $("#cell8").removeClass("playing");
-    }else if(event['key']==='9'){
-            $("#cell9").removeClass("playing");
-    }
+        if(Number(event['key'])>=1&&Number(event['key'])<=9){
+                var cell = "#cell"+event['key'];	
+                $(cell).removeClass("playing");   
+        }
 }
